2b12d7e8314e751a39ca1802cf70ca2d2571c8c3
Kacper Rogowski <rogemus@gmail.com>
2025-12-10 21:37:00 +0100

    feat: conver css vars to values

--- themes/rogemus/postcss.config.js
+++
@@ -1,9 +0,0 @@
-const postcssNesting = require('postcss-nesting');
-const pruneVar = require('postcss-prune-var');
-
-module.exports = {
-  plugins: [
-    postcssNesting(),
-    pruneVar()
-  ]
-};
